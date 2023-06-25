96bff2eb60926724fbea582b425a3d0632698fd4
sinans <svrsinan06@gmail.com>
2023-06-25 22:17:14 +0300

    css2

--- maya/src/components/Main.jsx
+++ maya/src/components/Main.jsx
@@ -4,7 +4,7 @@ import Card from "./Card"
 const Main = ({data}) => {
     
   return (
-    <main className="rounded-3 ">
+    <main className="rounded-3 container mt-5">
         <Card data={data}/>
     </main>
   )
